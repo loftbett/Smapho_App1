587947021c4d4d3323afb5e27df8224a46c67329
loftbett <loftbett@gmail.com>
2020-11-02 10:21:59 +0900

    Todoのアプリのコピー

---
+++ sell_history/lib/models/todo.dart
@@ -0,0 +1,25 @@
+import '../components/importer.dart';
+
+class Todo {
+  String id;
+  String title;
+  DateTime dueDate;
+  String note;
+
+  Todo(this.title, this.dueDate, this.note);
+  Todo.newTodo() {
+    title = "";
+    dueDate = DateTime.now();
+    note = "";
+  }
+
+  Todo clone() {
+    Todo newTodo = Todo(title, dueDate, note);
+    newTodo.id = id;
+    return newTodo;
+  }
+
+  assignUUID() {
+    id = Uuid().v4();
+  }
+}
---
+++ sell_history/lib/repositories/todo_bloc.dart
@@ -0,0 +1,50 @@
+import '../components/importer.dart';
+
+// Todo管理用のBlocクラス
+class TodoBloc {
+  // 管理している全Todoリスト
+  static final List<Todo> sampleTodos = [];
+
+  // BlocのStreamコントローラ
+  final _todoController = StreamController<List<Todo>>();
+
+  // 出力Streamのメソッドのgetter(Dartの場合は、get修飾子を使う。)
+  Stream<List<Todo>> get todoStream => _todoController.stream;
+
+  // sinkへの流しメソッド
+  getTodos() {
+    _todoController.sink.add(sampleTodos);
+  }
+
+// コンストラクタ
+  TodoBloc() {
+    // 新規作成時に新規Listをsinkへ流し込み
+    getTodos();
+  }
+
+  dispose() {
+    _todoController.close();
+  }
+
+  create(Todo todo) {
+    todo.assignUUID();
+    sampleTodos.add(todo);
+    getTodos();
+  }
+
+  update(Todo todo) {
+    int _index = sampleTodos.indexWhere((Todo t) => t.id == todo.id);
+    if (_index >= 0) {
+      sampleTodos[_index] = todo;
+      getTodos();
+    }
+  }
+
+  delete(String id) {
+    int _index = sampleTodos.indexWhere((Todo t) => t.id == id);
+    if (_index >= 0) {
+      sampleTodos.removeAt(_index);
+      getTodos();
+    }
+  }
+}
