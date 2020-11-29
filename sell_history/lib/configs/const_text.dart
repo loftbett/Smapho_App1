5451da548960cc3884b6f84c2c8458dc917df6aa
loftbett <loftbett@gmail.com>
2020-11-29 11:27:40 +0900

    定数情報追加

--- sell_history/lib/configs/const_text.dart
+++ sell_history/lib/configs/const_text.dart
@@ -1,6 +1,12 @@
+import 'package:intl/intl.dart';
+
 class ConstText {
   static final appTitle = "Simple Todo App";
   static final todoListView = "Todo List";
   static final todoEditView = "Todo Edit";
   static final todoCreateView = "New Todo";
+
+  static final dateFormatYMD = new DateFormat('yyyy-MM-dd');
+  static final dateFormatYM = new DateFormat('yyyy-MM');
+  static final dateFormatYMDHM = new DateFormat('yyyy-MM-dd H:m');
 }
---
+++ sell_history/lib/configs/style.dart
@@ -0,0 +1,11 @@
+import 'package:sell_history/components/importer.dart';
+
+class StyleBase {
+  TextStyle completedTodoStyle() {
+    return TextStyle(color: Colors.white);
+  }
+
+  TextStyle todoStyle() {
+    return TextStyle(color: Colors.black);
+  }
+}
