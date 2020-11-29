16c79d15a4e4d99b77a83c878dc18baaffa52ac0
loftbett <loftbett@gmail.com>
2020-11-29 11:27:17 +0900

    importer情報整理

--- sell_history/lib/components/importer.dart
+++ sell_history/lib/components/importer.dart
@@ -1,25 +1,31 @@
+// 基本コンポーネント
 export 'package:flutter/material.dart';
 export 'package:provider/provider.dart';
+export 'dart:io';
 
+/**
+ * プロジェクトクラス
+ */
+// 定数クラス
 export 'package:sell_history/configs/const_text.dart';
-export 'package:sell_history/repositories/todo_bloc.dart';
-
-export 'todo_list/todo_list_view.dart';
-
-export 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-//export 'package:intl/intl.dart';
-export 'package:sell_history/configs/const_text.dart';
-export 'package:sell_history/models/todo.dart';
-export 'package:sell_history/repositories/todo_bloc.dart';
-
-export 'package:provider/provider.dart';
+export 'package:sell_history/configs/style.dart';
+// Widgetクラス
 export 'package:sell_history/components/todo_edit/todo_edit_view.dart';
-export 'package:sell_history/configs/const_text.dart';
-export 'package:sell_history/models/todo.dart';
+export 'package:sell_history/components/todo_list/todo_list_view.dart';
+// データ管理クラス
 export 'package:sell_history/repositories/todo_bloc.dart';
+export 'package:sell_history/repositories/db_provider.dart';
+// Todoデータクラス
+export 'package:sell_history/models/todo.dart';
 
+/**
+ * 追加ライブラリ
+ */
+export 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 export 'package:uuid/uuid.dart';
-
 export 'dart:async';
 export 'dart:core';
-export 'package:sell_history/models/todo.dart';
+export 'package:path/path.dart';
+export 'package:path_provider/path_provider.dart';
+export 'package:sqflite/sqflite.dart';
+export 'package:sqflite/sqlite_api.dart';
